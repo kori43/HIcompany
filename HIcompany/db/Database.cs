063baf87981a43a1a910b784c2227be08009771a
Richard <94477473+kori43@users.noreply.github.com>
2024-09-05 23:05:34 +0500

    add specification

--- HIcompany/db/Database.cs
+++ HIcompany/db/Database.cs
@@ -11,21 +11,21 @@ namespace HIcompany.db
     public class Database
     {
         SqlConnection sqlConnection = new SqlConnection(@"Data Source=;Initial Catalog=HIcompany;Integrated Security=True");
-        public void openConnection()
+        public void OpenConnection()
         {
             if (sqlConnection.State == ConnectionState.Closed)
             {
                 sqlConnection.Open();
             }
         }
-        public void closeConnection()
+        public void CloseConnection()
         {
             if (sqlConnection.State == ConnectionState.Open)
             {
                 sqlConnection.Close();
             }
         }
-        public SqlConnection getConnection()
+        public SqlConnection GetConnection()
         {
             return sqlConnection;
         }
