00485db33405df854ac4166c8635c689aebf57b4
Richard <94477473+kori43@users.noreply.github.com>
2024-09-05 22:47:57 +0500

    add sql script, text doc, class db

---
+++ HIcompany/db/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HIcompany.db
+{
+    public class Database
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=;Initial Catalog=HIcompany;Integrated Security=True");
+        public void openConnection()
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
