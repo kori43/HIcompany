fe23b578f5daeb4565464c90c0dcd41e5b16aac3
Richard <94477473+kori43@users.noreply.github.com>
2024-09-07 17:46:11 +0500

    added processing of button clicks in signup

--- HIcompany/Pages/SignUp_Win.xaml.cs
+++ HIcompany/Pages/SignUp_Win.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System.Windows;
+using System.Data.SqlClient;
+using HIcompany.db;
+using System.Data;
 
 namespace HIcompany.Pages
 {
     public partial class SignUp_Win : Window
     {
-
+        Database database = new Database();
         public SignUp_Win()
         {
             InitializeComponent();
@@ -12,14 +15,38 @@ namespace HIcompany.Pages
 
         private void Btn_Create_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CheckUser())
+                return;
+            string firstName = TextBox_FirstName.Text;
+            string lastName = TextBox_LastName.Text;
+            DateTime date = Convert.ToDateTime(DatePicker.Text);
+            string phone = TextBox_Phone.Text;
+            try
+            {
+                string query = $"INSERT INTO Clients(FirstName, LastName, DateOfBirth, Phone) VALUES ('{firstName}', '{lastName}', '{date}', '{phone}')";
+                SqlCommand command = new SqlCommand(query, database.GetConnection());
+                database.OpenConnection();
+                if (firstName == "" || lastName == "" || date == null || phone == "")
+                    MessageBox.Show("Не удалось зарегистрировать клиента!");
+                else
+                {
+                    if (command.ExecuteNonQuery() == 1)
+                        MessageBox.Show("Успешно!");
+                    else
+                        MessageBox.Show("Не удалось зарегистрировать клиента!");
+                }
+                database.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }         
         }
 
         private void Btn_Clear_Click(object sender, RoutedEventArgs e)
         {
             TextBox_FirstName.Clear();
             TextBox_LastName.Clear();
-            TextBox_DateOfBirth.Clear();
             TextBox_Phone.Clear();
         }
 
@@ -27,6 +54,33 @@ namespace HIcompany.Pages
         {
             // todo возврат на окно оператора
             this.Close();
+        }
+
+        private bool CheckUser()
+        {
+            string firstname = TextBox_FirstName.Text;
+            string lastName = TextBox_LastName.Text;
+            DateTime dateofbirth = Convert.ToDateTime(DatePicker.Text);
+            string phone = TextBox_Phone.Text;
+            
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            string query = $"SELECT * from Clients WHERE Phone = '{phone}'";
+
+            SqlCommand command = new SqlCommand(query, database.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0)
+            {
+                MessageBox.Show("Такой клиент уже существует!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
